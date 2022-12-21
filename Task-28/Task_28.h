df716c02c66fd6fb61fe55753d29e866d47964ee
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-12-21 19:03:43 +0300

    Initial Commit

---
+++ Task-28/Task_28.h
@@ -0,0 +1,18 @@
+//
+//  Task_28.h
+//  Task-28
+//
+//  Created by Егор Бадмаев on 21.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Task_28.
+FOUNDATION_EXPORT double Task_28VersionNumber;
+
+//! Project version string for Task_28.
+FOUNDATION_EXPORT const unsigned char Task_28VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Task_28/PublicHeader.h>
+
+
